52325bcbe7db0282902dd3ab2ef7def9b8142484
tong <zhao.1991@osu.edu>
2023-04-08 09:55:36 -0500

    adding simple chess app

--- app_.py
+++ app_.py
@@ -10,10 +10,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        move = request.form["animal"]
+        animal = request.form["animal"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(move),
+            prompt=generate_prompt(animal),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,7 +22,14 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(move):
-    return f"""Provide a valid chess move in response to the following input move:
+def generate_prompt(animal):
+    return """Suggest three names for an animal that is a superhero.
 
-Input move: {move}"""
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: {}
+Names:""".format(
+        animal.capitalize()
+    )
